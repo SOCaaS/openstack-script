7091d7b91bc875ab40a70ebcbb84c520761b01ea
GeorgiusKurli <georgius.e.k@gmail.com>
2020-10-17 12:01:37 +0700

    keystone.sql and install keystone

--- keystone/start.sh
+++ keystone/start.sh
@@ -1,2 +1,15 @@
-echo lol
+apt update
+apt upgrade -y
 echo $(grep MYSQL_HOST .env | cut -d '=' -f2)
+
+# run keystone.sql
+mysql -u root -p < keystone.sql
+
+# Install keystone
+echo "Installing keystone"
+apt install -y keystone
+
+# edit keystone.conf
+sed -i -e "s|^connection = .*|connection = mysql+pymysql://keystone:KEYSTONE_DBPASS@controller/keystone|g" /etc/keystone/keystone.conf
+
+sed -i -e "s|^provider = .*|provider = fernet|g" /etc/keystone/keystone.conf
