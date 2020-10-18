bb507a92f397aff1fcd9907f86d16b1d713aa4a6
GeorgiusKurli <georgius.e.k@gmail.com>
2020-10-18 16:26:09 +0700

    add user sql

--- keystone/keystone.sql
+++ keystone/keystone.sql
@@ -1,8 +1,12 @@
 CREATE DATABASE  keystone;
 
+CREATE USER 'keystone'@'localhost'
+
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' \
 IDENTIFIED BY 'NCQhEHRb';
 
+/*
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' \
 IDENTIFIED BY 'NCQhEHRb';
+*/
 
--- keystone/start.sh
+++ keystone/start.sh
@@ -1,6 +1,6 @@
+set -e
 apt update
 apt upgrade -y
-echo $(grep MYSQL_HOST .env | cut -d '=' -f2)
 
 # run keystone.sql
 mysql -e "source keystone.sql";
@@ -10,7 +10,7 @@ echo "Installing keystone"
 apt install -y keystone
 
 # edit keystone.conf
-sed -i -e "s|^connection = .*|connection = mysql+pymysql://keystone:KEYSTONE_DBPASS@controller/keystone|g" /etc/keystone/keystone.conf
+sed -i -e "s|^connection = .*|connection = mysql+pymysql://keystone:NCQhEHRb@controller/keystone|g" /etc/keystone/keystone.conf
 
 sed -i -e "s|^provider = .*|provider = fernet|g" /etc/keystone/keystone.conf
 
