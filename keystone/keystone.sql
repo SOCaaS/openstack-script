a1b5989e875c9be3dab4d991423a6a852e0f42bf
GeorgiusKurli <georgius.e.k@gmail.com>
2020-10-18 16:34:21 +0700

    remove redundant identified by

--- keystone/keystone.sql
+++ keystone/keystone.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS keystone;
 CREATE USER 'keystone'@'localhost' IDENTIFIED BY 'NCQhEHRb';
 CREATE USER 'keystone'@'%' IDENTIFIED BY 'NCQhEHRb';
 
-GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'NCQhEHRb';
-GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'NCQhEHRb';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%';
 
 
