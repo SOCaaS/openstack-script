65eddfe84ae1769032596563f3fd29492b945ea3
GeorgiusKurli <georgius.e.k@gmail.com>
2020-10-18 15:50:53 +0700

    fixes

--- keystone/start.sh
+++ keystone/start.sh
@@ -10,7 +10,7 @@ echo "Installing keystone"
 apt install -y keystone
 
 # edit keystone.conf
-sed -i -e "s|^connection = .*|connection = mysql+pymysql://keystone:KEYSTONE_DBPASS@controller/keystone|g" /etc/keystone/keystone.conf
+sed -i -e "s|^connection = .*|connection = mysql+pymysql://keystone:NCQhEHRb@controller/keystone|g" /etc/keystone/keystone.conf
 
 sed -i -e "s|^provider = .*|provider = fernet|g" /etc/keystone/keystone.conf
 
@@ -34,7 +34,7 @@ sed -i -e "s|^ServerName .*|ServerName controller|g" /etc/apache2/apache2.conf
 # restart apache server
 service apache2 restart
 
-# set env variables
+# set env variables	
 $ export OS_USERNAME=admin
 $ export OS_PASSWORD=ADMIN_PASS
 $ export OS_PROJECT_NAME=admin
@@ -42,3 +42,24 @@ $ export OS_USER_DOMAIN_NAME=Default
 $ export OS_PROJECT_DOMAIN_NAME=Default
 $ export OS_AUTH_URL=http://controller:5000/v3
 $ export OS_IDENTITY_API_VERSION=3
+
+# install openstack train
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
