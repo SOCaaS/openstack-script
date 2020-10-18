9728f4628fe09e1f151ccfd76ea1c6974642e1f3
GeorgiusKurli <georgius.e.k@gmail.com>
2020-10-18 16:30:27 +0700

    sql keystone user fix

--- keystone/keystone.sql
+++ keystone/keystone.sql
@@ -1,12 +1,9 @@
-CREATE DATABASE  keystone;
+CREATE DATABASE IF NOT EXISTS keystone;
 
-CREATE USER 'keystone'@'localhost'
+CREATE USER 'keystone'@'localhost' IDENTIFIED BY 'NCQhEHRb';
+CREATE USER 'keystone'@'%' IDENTIFIED BY 'NCQhEHRb';
 
-GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' \
-IDENTIFIED BY 'NCQhEHRb';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'NCQhEHRb';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'NCQhEHRb';
 
-/*
-GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' \
-IDENTIFIED BY 'NCQhEHRb';
-*/
 
