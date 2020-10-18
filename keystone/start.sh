cd4314c1cef5520d9d6e3f7e8abe36e038c27fd1
GeorgiusKurli <georgius.e.k@gmail.com>
2020-10-18 17:34:26 +0700

    echo steps

--- keystone/start.sh
+++ keystone/start.sh
@@ -10,31 +10,41 @@ echo "Installing keystone"
 apt install -y keystone
 
 # edit keystone.conf
+echo "editing keystone.conf"
 sed -i -e "s|^connection = .*|connection = mysql+pymysql://keystone:NCQhEHRb@controller/keystone|g" /etc/keystone/keystone.conf
 
 sed -i -e '/\[token\]/a\' -e 'provider = fernet' /etc/keystone/keystone.conf
 
 # Populate the Identity service database
+echo "populating the identity service database"
 su -s /bin/sh -c "keystone-manage db_sync" keystone
 
 # Initialize Fernet key repositories
+echo "initializing Fernet key repositories"
 keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
 keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
 
 # Bootstrap the Identity service
+echo "bootstrap identity services"
 keystone-manage bootstrap --bootstrap-password 9zExzZzL \
   --bootstrap-admin-url http://controller:5000/v3/ \
   --bootstrap-internal-url http://controller:5000/v3/ \
   --bootstrap-public-url http://controller:5000/v3/ \
   --bootstrap-region-id RegionOne
 
+
 # set servername to controller
+echo "Servername set to controller"
 sed -i -e "s|^ServerName .*|ServerName controller|g" /etc/apache2/apache2.conf
 
+
 # restart apache server
+echo "Restarting apache2"
 service apache2 restart
 
+
 # set env variables	
+echo "setting env variables"
 $ export OS_USERNAME=admin
 $ export OS_PASSWORD=9zExzZzL
 $ export OS_PROJECT_NAME=admin
