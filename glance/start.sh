216680d20c47f14a175f37296712a183748c49dd
ivanralali <ivan.suratno@ralali.com>
2020-10-31 16:38:22 +0700

    Update and fix sed using the starting only part

--- glance/start.sh
+++ glance/start.sh
@@ -36,16 +36,20 @@ apt install -y glance
 echo -e "\nEditing glance-api.conf"
 sed -i -e "s|^connection = .*|connection = mysql+pymysql://glance:837ruyDA312y23djs@controller/glance|g" /etc/glance/glance-api.conf
 
-sed -i -e '/\[keystone_authtoken\]/a\' -e "www_authenticate_uri = http://controller:5000" /etc/glance/glance-api.conf
-sed -i -e '/\[keystone_authtoken\]/a\' -e "memcached_servers = controller:11211" /etc/glance/glance-api.conf
-sed -i -e '/\[keystone_authtoken\]/a\' -e "auth_type = password" /etc/glance/glance-api.conf
-sed -i -e '/\[keystone_authtoken\]/a\' -e "project_domain_name = Default" /etc/glance/glance-api.conf
-sed -i -e '/\[keystone_authtoken\]/a\' -e "user_domain_name = Default" /etc/glance/glance-api.conf
-sed -i -e '/\[keystone_authtoken\]/a\' -e "project_name = service" /etc/glance/glance-api.conf
-sed -i -e '/\[keystone_authtoken\]/a\' -e "username = glance" /etc/glance/glance-api.conf
-sed -i -e '/\[keystone_authtoken\]/a\' -e "password = yrgehdbsjkhu32897124" /etc/glance/glance-api.conf
+sed -i -e '/^\[keystone_authtoken\]/a\' -e "www_authenticate_uri = http://controller:5000" /etc/glance/glance-api.conf
+sed -i -e '/^\[keystone_authtoken\]/a\' -e "memcached_servers = controller:11211" /etc/glance/glance-api.conf
+sed -i -e '/^\[keystone_authtoken\]/a\' -e "auth_type = password" /etc/glance/glance-api.conf
+sed -i -e '/^\[keystone_authtoken\]/a\' -e "project_domain_name = Default" /etc/glance/glance-api.conf
+sed -i -e '/^\[keystone_authtoken\]/a\' -e "user_domain_name = Default" /etc/glance/glance-api.conf
+sed -i -e '/^\[keystone_authtoken\]/a\' -e "project_name = service" /etc/glance/glance-api.conf
+sed -i -e '/^\[keystone_authtoken\]/a\' -e "username = glance" /etc/glance/glance-api.conf
+sed -i -e '/^\[keystone_authtoken\]/a\' -e "password = yrgehdbsjkhu32897124" /etc/glance/glance-api.conf
 
-sed -i -e '/\[paste_deploy\]/a\' -e 'flavor = keystone' /etc/glance/glance-api.conf
+sed -i -e '/^\[paste_deploy\]/a\' -e 'flavor = keystone' /etc/glance/glance-api.conf
+
+sed -i -e '/^\[glance_store\]/a\' -e 's|^stores = .*|stores = file,http|g' /etc/glance/glance-api.conf
+sed -i -e '/^\[glance_store\]/a\' -e 'default_store = file' /etc/glance/glance-api.conf
+sed -i -e '/^\[glance_store\]/a\' -e 'filesystem_store_datadir = /var/lib/glance/images/' /etc/glance/glance-api.conf
 
 echo -e "\nDB Sync Glance"
 set +e
