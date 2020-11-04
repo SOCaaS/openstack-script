6af603908e97afd267751d12ead6c3ad51c18134
GeorgiusKurli <georgius.e.k@gmail.com>
2020-11-04 11:56:33 +0700

    add apt update after adding dependencies

--- Openstack-Base/start.sh
+++ Openstack-Base/start.sh
@@ -24,21 +24,22 @@ echo "installing rabbitMQ"
 
 echo "installing erlang"
 apt-get install -y wget
-wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo apt-key add -
-echo "deb https://packages.erlang-solutions.com/ubuntu focal contrib" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | apt-key add -
+echo "deb https://packages.erlang-solutions.com/ubuntu focal contrib" | tee /etc/apt/sources.list.d/rabbitmq.list
 
 apt install -y erlang
 echo "install rabbitMQ"
 #add rabbitmq Repository to Ubuntu
 apt install -y apt-transport-https
-wget -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | sudo apt-key add -
-wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
-echo "deb https://dl.bintray.com/rabbitmq-erlang/debian focal erlang-22.x" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
-suto apt install -y rabbitmq-server
+wget -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | apt-key add -
+wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | apt-key add -
+echo "deb https://dl.bintray.com/rabbitmq-erlang/debian focal erlang-22.x" | tee /etc/apt/sources.list.d/rabbitmq.list
+apt update
+apt install -y rabbitmq-server
 systemctl status rabbitmq-server.service
 
 rabbitmq-plugins enable rabbitmq_management
-sudo ufw allow proto tcp from any to any port 5672,15672
+ufw allow proto tcp from any to any port 5672,15672
 
 rabbitmqctl add_user admin HELP123
 rabbitmqctl set_user_tags admin administrator
