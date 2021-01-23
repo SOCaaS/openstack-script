75a7c2a655c2b1ba1c887b148957127f0b1d87b6
ezeutno <ivan.suratno@gmail.com>
2021-01-23 11:46:54 +0700

    Update Openstack Base

--- openstack-base/start.sh
+++ openstack-base/start.sh
@@ -2,34 +2,36 @@ set -e
 apt update
 apt upgrade -y
 
-echo "Install Net-Tools"
+echo -e "Install Net-Tools"
 apt install -y net-tools
 
-echo "Install Openstack Client"
+echo -e "Install Openstack Client"
 snap install openstackclients --classic
 
-echo "Installing Mysql"
+echo -e "Installing Mysql"
 apt install -y mysql-server
 
 service mysql status
 
-echo "Install memcache server"
+echo -e "Install memcache server"
 apt install -y memcached
 
-echo -e "\nEcho to /etc/hosts add controller"
+echo -e "\nEcho to /etc/hosts add DEFAULT_URL"
 
-echo -e "\n127.0.0.1 controller" >> /etc/hosts
+echo -e "\n$(grep HOST_IP ../.env | cut -d '=' -f2) $(grep DEFAULT_URL ../.env | cut -d '=' -f2)" >> /etc/hosts
 
-echo "installing rabbitMQ"
+echo -e "Installing RabbitMQ"
 
-echo "installing erlang"
+echo -e "Installing Erlang"
+
 apt-get install -y wget
 wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | apt-key add -
 echo "deb https://packages.erlang-solutions.com/ubuntu focal contrib" | tee /etc/apt/sources.list.d/rabbitmq.list
 apt update
 
 apt install -y erlang
-echo "install rabbitMQ"
+
+echo -e"Install RabbitMQ"
 #add rabbitmq Repository to Ubuntu
 apt install -y apt-transport-https
 wget -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | apt-key add -
@@ -37,10 +39,15 @@ wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | apt-key add
 echo "deb https://dl.bintray.com/rabbitmq-erlang/debian focal erlang-22.x" | tee /etc/apt/sources.list.d/rabbitmq.list
 apt update
 apt install -y rabbitmq-server
+
 systemctl status rabbitmq-server.service
 
 rabbitmq-plugins enable rabbitmq_management
 
-rabbitmqctl add_user admin r32uhdejnkaskj
-rabbitmqctl set_user_tags admin administrator
-rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
+# set rabbitmq details
+rabbitmqctl add_user $(grep rabbitMQ_USER ../.env | cut -d '=' -f2) $(grep rabbitMQ_PASSWORD ../.env | cut -d '=' -f2)
+rabbitmqctl set_user_tags $(grep rabbitMQ_USER ../.env | cut -d '=' -f2) administrator
+rabbitmqctl set_permissions -p $(grep rabbitMQ_PATH ../.env | cut -d '=' -f2) $(grep rabbitMQ_USER ../.env | cut -d '=' -f2) ".*" ".*" ".*"
+
+echo -e "Install Crudini"
+apt install -y crudini
