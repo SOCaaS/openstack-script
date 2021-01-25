da9068a4a29aec7c2f275c9492c44131156ca8a0
ezeutno <ivan.suratno@gmail.com>
2021-01-25 22:42:39 +0700

    Add init on Installation

--- integration/start.sh
+++ integration/start.sh
@@ -1,5 +1,12 @@
+set -e
+
+echo -e "\nInit"
+apt update
+apt upgrade -y
+apt install -y python3-pip
+
 echo -e "\nInstall Tailon"
-pip install tailon
+pip3 install tailon
 
 echo -e "\nRun Tailon"
 nohup tailon -p basic -u admin:whenguardian2021 -b 0.0.0.0:9098 -f /var/log/ &
