f537c189dc1c6373430b00f0cb167bcf8177eaaa
ezeutno <ivan.suratno@gmail.com>
2021-01-25 23:00:18 +0700

    Destroy Instance

--- deployment/main.tf
+++ deployment/main.tf
@@ -7,10 +7,10 @@ terraform {
 module "digitalocean" {
     source              = "./droplet"
     servers             = [
-        {
-            name = "script-openstack-test",
-            type = "s-4vcpu-8gb"
-        }
+        # {
+        #     name = "script-openstack-test",
+        #     type = "s-4vcpu-8gb"
+        # }
     ]
     public_key_name     = var.public_key_name
     digital_ocean_key   = var.digital_ocean_key
