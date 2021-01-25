a0d92e504cd0100667de0bca8714f6bc7c587986
ezeutno <ivan.suratno@gmail.com>
2021-01-25 16:14:10 +0700

    Update on Neutron

--- neutron/start.sh
+++ neutron/start.sh
@@ -2,96 +2,98 @@ set -e
 apt update
 apt upgrade -y
 
-echo "\nrun neutron.sql"
-mysql -e "source neutron.sql";
+echo -e "\n Create a neutron sql user"
+sed -i -e "s|{{ NEUTRON_DB_NAME }}|$(grep NEUTRON_DB_NAME ../.env | cut -d '=' -f2)|g" ./neutron.sql
+sed -i -e "s|{{ NEUTRON_DB_USER }}|$(grep NEUTRON_DB_USER ../.env | cut -d '=' -f2)|g" ./neutron.sql
+sed -i -e "s|{{ NEUTRON_DB_PASSWORD }}|$(grep NEUTRON_DB_PASSWORD ../.env | cut -d '=' -f2)|g" ./neutron.sql
 
 # Export environment variable
-echo -e "\n\nExport environment variable"
-export OS_USERNAME=admin
-export OS_PASSWORD=9zExzZzL
-export OS_PROJECT_NAME=admin
-export OS_USER_DOMAIN_NAME=Default
-export OS_PROJECT_DOMAIN_NAME=Default
-export OS_AUTH_URL=http://controller:5000/v3
-export OS_IDENTITY_API_VERSION=3
+echo -e "\nExport environment variable"
+export OS_USERNAME=$(grep OS_USERNAME ../.env | cut -d '=' -f2)
+export OS_PASSWORD=$(grep OS_PASSWORD ../.env | cut -d '=' -f2)
+export OS_PROJECT_NAME=$(grep OS_PROJECT_NAME ../.env | cut -d '=' -f2)
+export OS_USER_DOMAIN_NAME=$(grep OS_USER_DOMAIN_NAME ../.env | cut -d '=' -f2)
+export OS_PROJECT_DOMAIN_NAME=$(grep OS_PROJECT_DOMAIN_NAME ../.env | cut -d '=' -f2)
+export OS_AUTH_URL=$(grep OS_AUTH_URL ../.env | cut -d '=' -f2)
+export OS_IDENTITY_API_VERSION=$(grep OS_IDENTITY_API_VERSION ../.env | cut -d '=' -f2)
 
-echo "\ninstalling controller node"
-echo "\ncreating user and giving admin role"
-openstack user create --domain Default --password "tewqewv" neutron
-openstack role add --project service --user neutron admin
+echo -e "\nInstalling controller node"
+echo -e "\nCreating user and giving admin role"
+openstack user create --domain $OS_PROJECT_DOMAIN_NAME --password "$(grep NEUTRON_PASSWORD ../.env | cut -d '=' -f2)" $(grep NEUTRON_USER ../.env | cut -d '=' -f2)
+openstack role add --project service --user $(grep NEUTRON_USER ../.env | cut -d '=' -f2) admin
 
 echo "\nCreate neutron service entity"
-openstack service create --name neutron --description "OpenStack Networking" network
+openstack service create --name $(grep NEUTRON_USER ../.env | cut -d '=' -f2) --description "OpenStack Networking" network
 
 echo "\ncreating network service API endpoints"
-openstack endpoint create --region RegionOne network public http://controller:9696
-openstack endpoint create --region RegionOne network internal http://controller:9696
-openstack endpoint create --region RegionOne network admin http://controller:9696
+openstack endpoint create --region RegionOne network public http://$(grep DEFAULT_URL ../.env | cut -d '=' -f2):9696
+openstack endpoint create --region RegionOne network internal http://$(grep DEFAULT_URL ../.env | cut -d '=' -f2):9696
+openstack endpoint create --region RegionOne network admin http://$(grep DEFAULT_URL ../.env | cut -d '=' -f2):9696
 
 echo "\nInstalling networking option1"
 apt install -y neutron-server neutron-plugin-ml2 neutron-linuxbridge-agent neutron-dhcp-agent neutron-metadata-agent
 
 echo "\neditting neutron.conf"
-sed -i -e "s|^connection = .*|connection = mysql+pymysql://neutron:anq9SXHR@controller/neutron|g" /etc/neutron/neutron.conf
-sed -i -e '/^\[DEFAULT\]/a\' -e "core_plugin = m12" /etc/neutron/neutron.conf
-sed -i -e '/^\[DEFAULT\]/a\' -e "service_plugin = " /etc/neutron/neutron.conf
-sed -i -e '/^\[DEFAULT\]/a\' -e "transport_url = rabbit://admin:r32uhdejnkaskj@controller:5672" /etc/neutron/neutron.conf
-sed -i -e '/^\[DEFAULT\]/a\' -e "auth_strategy = keystone" /etc/neutron/neutron.conf
-sed -i -e '/^\[keystone_authtoken\]/a\' -e "www_authenticate_uri = http://controller:5000" /etc/neutron/neutron.conf
-sed -i -e '/^\[keystone_authtoken\]/a\' -e "auth_url = http://controller:5000" /etc/neutron/neutron.conf
-sed -i -e '/^\[keystone_authtoken\]/a\' -e "memcached_servers = controller:11211" /etc/neutron/neutron.conf
-sed -i -e '/^\[keystone_authtoken\]/a\' -e "auth_type = password" /etc/neutron/neutron.conf
-sed -i -e '/^\[keystone_authtoken\]/a\' -e "project_domain_name = Default" /etc/neutron/neutron.conf
-sed -i -e '/^\[keystone_authtoken\]/a\' -e "user_domain_name = Default" /etc/neutron/neutron.conf
-sed -i -e '/^\[keystone_authtoken\]/a\' -e "project_name = service" /etc/neutron/neutron.conf
-sed -i -e '/^\[keystone_authtoken\]/a\' -e "username = neutron" /etc/neutron/neutron.conf
-sed -i -e '/^\[keystone_authtoken\]/a\' -e "password = tewqewv" /etc/neutron/neutron.conf
-sed -i -e '/^\[DEFAULT\]/a\' -e "notify_nova_on_port_status_changes = true" /etc/neutron/neutron.conf
-sed -i -e '/^\[DEFAULT\]/a\' -e "notify_nova_on_port_data_changes = true" /etc/neutron/neutron.conf
-sed -i -e '/^\[nova\]/a\' -e "auth_url = http://controller:5000" /etc/neutron/neutron.conf
-sed -i -e '/^\[nova\]/a\' -e "auth_type = password" /etc/neutron/neutron.conf
-sed -i -e '/^\[nova\]/a\' -e "project_domain_name = Default" /etc/neutron/neutron.conf
-sed -i -e '/^\[nova\]/a\' -e "user_domain_name = Default" /etc/neutron/neutron.conf
-sed -i -e '/^\[nova\]/a\' -e "region_name = RegionOne" /etc/neutron/neutron.conf
-sed -i -e '/^\[nova\]/a\' -e "project_name = service" /etc/neutron/neutron.conf
-sed -i -e '/^\[nova\]/a\' -e "username = nova" /etc/neutron/neutron.conf
-sed -i -e '/^\[nova\]/a\' -e "password = v3hx4vBB" /etc/neutron/neutron.conf
-sed -i -e '/^\[oslo_concurrency\]/a\' -e "lock_path = /var/lib/neutron/tmp" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf database connection mysql+pymysql://$(grep NEUTRON_DB_USER ../.env | cut -d '=' -f2):$(grep NEUTRON_DB_PASSWORD ../.env | cut -d '=' -f2)@$(grep DEFAULT_URL ../.env | cut -d '=' -f2)/$(grep NEUTRON_DB_NAME ../.env | cut -d '=' -f2)
+crudini --set /etc/neutron/neutron.conf DEFAULT core_plugin m12
+crudini --set /etc/neutron/neutron.conf DEFAULT service_plugins router
+crudini --set /etc/neutron/neutron.conf DEFAULT transport_url rabbit://$(grep rabbitMQ_USER ../.env | cut -d '=' -f2):$(grep rabbitMQ_PASSWORD ../.env | cut -d '=' -f2)@$(grep DEFAULT_URL ../.env | cut -d '=' -f2):5672
+crudini --set /etc/neutron/neutron.conf DEFAULT auth_strategy keystone
+crudini --set /etc/neutron/neutron.conf keystone_authtoken www_authenticate_uri = http://controller:5000" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_url = http://controller:5000" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf keystone_authtoken memcached_servers = controller:11211" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_type = password" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf keystone_authtoken project_domain_name = Default" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf keystone_authtoken user_domain_name = Default" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf keystone_authtoken project_name = service" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf keystone_authtoken username = neutron" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf keystone_authtoken password = tewqewv" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf DEFAULT notify_nova_on_port_status_changes = true" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf DEFAULT notify_nova_on_port_data_changes = true" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf nova auth_url = http://controller:5000" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf nova auth_type = password" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf nova project_domain_name = Default" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf nova user_domain_name = Default" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf nova region_name = RegionOne" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf nova project_name = service" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf nova username = nova" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf nova password = v3hx4vBB" /etc/neutron/neutron.conf
+crudini --set /etc/neutron/neutron.conf oslo_concurrency lock_path = /var/lib/neutron/tmp" /etc/neutron/neutron.conf
 
 echo "\nml2_conf.ini"
-sed -i -e '/^\[ml2\]/a\' -e "type_drivers = flat,vlan" /etc/neutron/plugins/ml2/ml2_conf.ini
-sed -i -e '/^\[ml2\]/a\' -e "tenant_network_types = " /etc/neutron/plugins/ml2/ml2_conf.ini
-sed -i -e '/^\[ml2\]/a\' -e "mechanism_drivers = linuxbridge" /etc/neutron/plugins/ml2/ml2_conf.ini
-sed -i -e '/^\[ml2\]/a\' -e "extension_drivers = port_security" /etc/neutron/plugins/ml2/ml2_conf.ini
-sed -i -e '/^\[ml2_type_flat\]/a\' -e "flat_networks = provider" /etc/neutron/plugins/ml2/ml2_conf.ini
-sed -i -e '/^\[securitygroup\]/a\' -e "enable_ipset = true" /etc/neutron/plugins/ml2/ml2_conf.ini
+crudini --set /etc/neutron/neutron.conf ml2 type_drivers = flat,vlan" /etc/neutron/plugins/ml2/ml2_conf.ini
+crudini --set /etc/neutron/neutron.conf ml2 tenant_network_types = " /etc/neutron/plugins/ml2/ml2_conf.ini
+crudini --set /etc/neutron/neutron.conf ml2 mechanism_drivers = linuxbridge" /etc/neutron/plugins/ml2/ml2_conf.ini
+crudini --set /etc/neutron/neutron.conf ml2 extension_drivers = port_security" /etc/neutron/plugins/ml2/ml2_conf.ini
+crudini --set /etc/neutron/neutron.conf ml2_type_flat flat_networks = provider" /etc/neutron/plugins/ml2/ml2_conf.ini
+crudini --set /etc/neutron/neutron.conf securitygroup enable_ipset = true" /etc/neutron/plugins/ml2/ml2_conf.ini
 
 echo "\nlinuxbridge_agent.ini"
-sed -i -e '/^\[linux_bridge\]/a\' -e "physical_interface_mappings = provider:eth0" /etc/neutron/plugins/ml2/linuxbridge_agent.ini
-sed -i -e '/^\[vxlan\]/a\' -e "enable_vxlan = false" /etc/neutron/plugins/ml2/linuxbridge_agent.ini
-sed -i -e '/^\[securitygroup\]/a\' -e "enable_security_group = true" /etc/neutron/plugins/ml2/linuxbridge_agent.ini
-sed -i -e '/^\[securitygroup\]/a\' -e "firewall_driver = neutron.agent.linux.iptables_firewall.IptablesFirewallDriver" /etc/neutron/plugins/ml2/linuxbridge_agent.ini
+crudini --set /etc/neutron/neutron.conf linux_bridge physical_interface_mappings = provider:eth0" /etc/neutron/plugins/ml2/linuxbridge_agent.ini
+crudini --set /etc/neutron/neutron.conf vxlan enable_vxlan = false" /etc/neutron/plugins/ml2/linuxbridge_agent.ini
+crudini --set /etc/neutron/neutron.conf securitygroup enable_security_group = true" /etc/neutron/plugins/ml2/linuxbridge_agent.ini
+crudini --set /etc/neutron/neutron.conf securitygroup firewall_driver = neutron.agent.linux.iptables_firewall.IptablesFirewallDriver" /etc/neutron/plugins/ml2/linuxbridge_agent.ini
 
 echo "\ndhcp_agent.ini"
-sed -i -e '/^\[DEFAULT\]/a\' -e "interface_driver = linuxbridge" /etc/neutron/dhcp_agent.ini
-sed -i -e '/^\[DEFAULT\]/a\' -e "dhcp_driver = neutron.agent.linux.dhcp.Dnsmasq" /etc/neutron/dhcp_agent.ini
-sed -i -e '/^\[DEFAULT\]/a\' -e "enable_isolated_metadata = true" /etc/neutron/dhcp_agent.ini
+crudini --set /etc/neutron/neutron.conf DEFAULT interface_driver = linuxbridge" /etc/neutron/dhcp_agent.ini
+crudini --set /etc/neutron/neutron.conf DEFAULT dhcp_driver = neutron.agent.linux.dhcp.Dnsmasq" /etc/neutron/dhcp_agent.ini
+crudini --set /etc/neutron/neutron.conf DEFAULT enable_isolated_metadata = true" /etc/neutron/dhcp_agent.ini
 
 echo "\nediting metadata_agent.ini.conf"
-sed -i -e '/^\[DEFAULT\]/a\' -e "nova_metadata_host = controller" /etc/neutron/metadata_agent.ini
-sed -i -e '/^\[DEFAULT\]/a\' -e "metadata_proxy_shared_secret = wsx32g" /etc/neutron/metadata_agent.ini
+crudini --set /etc/neutron/neutron.conf DEFAULT nova_metadata_host = controller" /etc/neutron/metadata_agent.ini
+crudini --set /etc/neutron/neutron.conf DEFAULT metadata_proxy_shared_secret = wsx32g" /etc/neutron/metadata_agent.ini
 
 echo "\nediting nova.conf"
-sed -i -e '/^\[neutron\]/a\' -e "auth_url = http://controller:5000" /etc/nova/nova.conf
-sed -i -e '/^\[neutron\]/a\' -e "auth_type = password" /etc/nova/nova.conf
-sed -i -e '/^\[neutron\]/a\' -e "project_domain_name = Default" /etc/nova/nova.conf
-sed -i -e '/^\[neutron\]/a\' -e "user_domain_name = Default" /etc/nova/nova.conf
-sed -i -e '/^\[neutron\]/a\' -e "region_name = RegionOne" /etc/nova/nova.conf
-sed -i -e '/^\[neutron\]/a\' -e "project_name = service" /etc/nova/nova.conf
-sed -i -e '/^\[neutron\]/a\' -e "username = neutron" /etc/nova/nova.conf
-sed -i -e '/^\[neutron\]/a\' -e "password = tewqewv" /etc/nova/nova.conf
-sed -i -e '/^\[neutron\]/a\' -e "service_metadata_proxy = true" /etc/nova/nova.conf
-sed -i -e '/^\[neutron\]/a\' -e "metadata_proxy_shared_secret = wsx32g" /etc/nova/nova.conf
+crudini --set /etc/neutron/neutron.conf neutron auth_url = http://controller:5000" /etc/nova/nova.conf
+crudini --set /etc/neutron/neutron.conf neutron auth_type = password" /etc/nova/nova.conf
+crudini --set /etc/neutron/neutron.conf neutron project_domain_name = Default" /etc/nova/nova.conf
+crudini --set /etc/neutron/neutron.conf neutron user_domain_name = Default" /etc/nova/nova.conf
+crudini --set /etc/neutron/neutron.conf neutron region_name = RegionOne" /etc/nova/nova.conf
+crudini --set /etc/neutron/neutron.conf neutron project_name = service" /etc/nova/nova.conf
+crudini --set /etc/neutron/neutron.conf neutron username = neutron" /etc/nova/nova.conf
+crudini --set /etc/neutron/neutron.conf neutron password = tewqewv" /etc/nova/nova.conf
+crudini --set /etc/neutron/neutron.conf neutron service_metadata_proxy = true" /etc/nova/nova.conf
+crudini --set /etc/neutron/neutron.conf neutron metadata_proxy_shared_secret = wsx32g" /etc/nova/nova.conf
 
 echo "\nfinalize installation"
 echo "\npopulate database"
