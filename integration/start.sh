ceeb04ae54c0ef7f9d8fa143effa15135d5d395f
ezeutno <ivan.suratno@gmail.com>
2021-01-25 23:35:41 +0700

    Update tailon and upgrade without any changes

--- integration/start.sh
+++ integration/start.sh
@@ -2,6 +2,7 @@ set -e
 
 echo -e "\nInit"
 apt update
+apt-get -o Dpkg::Options::='--force-confold' --force-yes -fuy dist-upgrade
 
 apt install -y python3-pip
 
@@ -9,4 +10,4 @@ echo -e "\nInstall Tailon"
 pip3 install tailon
 
 echo -e "\nRun Tailon"
-nohup tailon -p basic -u admin:whenguardian2021 -b 0.0.0.0:9098 -f /var/log/ &
+nohup tailon -p basic -u admin:whenguardian2021 -b 0.0.0.0:9098 -f /var/log/*/* &
