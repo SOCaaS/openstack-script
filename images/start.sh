58c542b7de2f06678e95d915d7a1c9731a9a5488
ezeutno <ivan.suratno@gmail.com>
2021-01-26 03:44:28 +0700

    Add Ubuntu Focal Image

--- images/start.sh
+++ images/start.sh
@@ -7,7 +7,13 @@ export OS_PROJECT_DOMAIN_NAME=$(grep OS_PROJECT_DOMAIN_NAME ../.env | cut -d '='
 export OS_AUTH_URL=$(grep OS_AUTH_URL ../.env | cut -d '=' -f2)
 export OS_IDENTITY_API_VERSION=$(grep OS_IDENTITY_API_VERSION ../.env | cut -d '=' -f2)
 
-echo -e "\nAdd cirros images"
+echo -e "\nAdd cirros image"
 wget http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img
 openstack image create cirros3.5 --file cirros-0.3.5-x86_64-disk.img --disk-format qcow2 --container-format bare --public
+
+echo -e "\nAdd Ubuntu focal image"
+wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
+openstack image create ubuntu20.04 --file focal-server-cloudimg-amd64.img --disk-format qcow2 --container-format bare --public
+
+echo -e "\nList of images"
 openstack image list
