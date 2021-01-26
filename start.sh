e6b938d0f0c8651945f5643256a68cffe75c54bb
ezeutno <ivan.suratno@gmail.com>
2021-01-27 05:58:03 +0700

    Add tailon from the start

--- start.sh
+++ start.sh
@@ -5,54 +5,72 @@ cd ./openstack-base
 ./start.sh
 cd ..
 
+echo -e "\nStart Integration Stack!"
+cd ./integration
+./start.sh
+cd ..
+
 echo -e "\nStart Keystone!"
 cd ./keystone
 ./start.sh
 cd ..
+
+systemctl restart tailon.service
 
 echo -e "\nStart Glance!"
 cd ./glance
 ./start.sh
 cd ..
 
+systemctl restart tailon.service
+
 echo -e "\nAdd Image!"
 cd ./images
 ./start.sh
 cd ..
+
+systemctl restart tailon.service
 
 echo -e "\nStart Placement!"
 cd ./placement
 ./start.sh
 cd ..
 
+systemctl restart tailon.service
+
 echo -e "\nStart Nova!"
 cd ./nova
 ./start.sh
 cd ..
+
+systemctl restart tailon.service
 
 echo -e "\nStart Neutron!"
 cd ./neutron
 ./start.sh
 cd ..
 
+systemctl restart tailon.service
+
 echo -e "\nStart Cinder!"
 cd ./cinder
 ./start.sh
 cd ..
+
+systemctl restart tailon.service
 
 echo -e "\nStart Horizon!"
 cd ./horizon
 ./start.sh
 cd ..
 
+systemctl restart tailon.service
+
 echo -e "\nAdd Flavors!"
 cd ./flavors
 ./start.sh
 cd ..
 
-echo -e "\nStart Integration Stack!"
-cd ./integration
-./start.sh
-cd ..
+systemctl restart tailon.service
 
 echo -e "Finish, all service has been deployed!"
