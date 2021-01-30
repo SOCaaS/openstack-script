b26ba823c5cad48d1ed8660d47602629f0509a31
ezeutno <ivan.suratno@gmail.com>
2021-01-30 10:48:29 +0700

    Fix falvor

--- flavors/start.sh
+++ flavors/start.sh
@@ -1,9 +1,6 @@
 set -e
 
-
-
-
 echo -e "\nExecute Flavor Creation"
-openstack flavor create --ram 512 --vcpus 1 --project service --project-domain $OS_PROJECT_DOMAIN_NAME s1.mini
+openstack flavor create --ram 512 --vcpus 1 --project service --private --project-domain $OS_PROJECT_DOMAIN_NAME s1.mini
 
 openstack flavor list
