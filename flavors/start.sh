b8e57558bef8b163582f48a13ea83ec2a7317626
ezeutno <ivan.suratno@gmail.com>
2021-01-30 09:43:12 +0700

    Fix flavor name

--- flavors/start.sh
+++ flavors/start.sh
@@ -11,6 +11,6 @@ export OS_IDENTITY_API_VERSION=$(grep OS_IDENTITY_API_VERSION ../.env | cut -d '
 
 
 echo -e "\nExecute Flavor Creation"
-openstack flavor create --ram 512mb --vcpus 1 --project service --project-domain $OS_PROJECT_DOMAIN_NAME
+openstack flavor create --ram 512 --vcpus 1 --project service --project-domain $OS_PROJECT_DOMAIN_NAME s1.mini
 
 openstack flavor list
