3baec4927ee14d8c6dcde7c5a4eb029b028609ad
ezeutno <ivan.suratno@gmail.com>
2021-01-30 08:40:58 +0700

    Add flavors through CLI

--- flavors/start.sh
+++ flavors/start.sh
@@ -1,1 +1,16 @@
 set -e
+
+echo -e "\nExport environment variable"
+export OS_USERNAME=$(grep OS_USERNAME ../.env | cut -d '=' -f2)
+export OS_PASSWORD=$(grep OS_PASSWORD ../.env | cut -d '=' -f2)
+export OS_PROJECT_NAME=$(grep OS_PROJECT_NAME ../.env | cut -d '=' -f2)
+export OS_USER_DOMAIN_NAME=$(grep OS_USER_DOMAIN_NAME ../.env | cut -d '=' -f2)
+export OS_PROJECT_DOMAIN_NAME=$(grep OS_PROJECT_DOMAIN_NAME ../.env | cut -d '=' -f2)
+export OS_AUTH_URL=$(grep OS_AUTH_URL ../.env | cut -d '=' -f2)
+export OS_IDENTITY_API_VERSION=$(grep OS_IDENTITY_API_VERSION ../.env | cut -d '=' -f2)
+
+
+echo -e "\nExecute Flavor Creation"
+openstack flavor create --ram 512mb --vcpus 1 --project service --project-domain $OS_PROJECT_DOMAIN_NAME
+
+openstack flavor list
