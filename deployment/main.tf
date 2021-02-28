527744d04dfad95c0b7df09a9a97ffe9433fc76b
ezeutno <ivan.suratno@gmail.com>
2021-03-01 05:14:47 +0700

    Turn off server

--- deployment/main.tf
+++ deployment/main.tf
@@ -7,10 +7,10 @@ terraform {
 module "digitalocean" {
     source              = "./droplet"
     servers             = [
-        {
-            name = "openstack-deployment-testing1",
-            type = "c-4"
-        }
+        # {
+        #     name = "openstack-deployment-testing1",
+        #     type = "c-4"
+        # }
     ]
     public_key_name     = var.public_key_name
     digital_ocean_key   = var.digital_ocean_key
